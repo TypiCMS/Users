c356877a7d562205d4ac932403cc0052b2c5da0f
Samuel De Backer <sdebacker@gmail.com>
2015-02-22 20:59:10 +0100

    nullable

--- src/database/migrations/2012_12_06_230000_add_preferences_to_users_table.php
+++ src/database/migrations/2012_12_06_230000_add_preferences_to_users_table.php
@@ -14,7 +14,7 @@ class AddPreferencesToUsersTable extends Migration
     {
         Schema::table('users', function(Blueprint $table)
         {
-            $table->text('preferences');
+            $table->text('preferences')->nullable();
         });
     }
 
