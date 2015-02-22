fa1508f1f01a8b7bfc9e3517cfa4a2a17538d47d
Samuel De Backer <sdebacker@gmail.com>
2015-02-22 20:59:37 +0100

    authcontroller

--- src/Http/Controllers/AuthController.php
+++ src/Http/Controllers/AuthController.php
@@ -8,49 +8,20 @@ use Illuminate\Mail\Message;
 use Input;
 use Mail;
 use Redirect;
-use TypiCMS\Http\Controllers\BaseAdminController;
+use Illuminate\Routing\Controller;
 use TypiCMS\Modules\Users\Http\Requests\FormRequest;
 use TypiCMS\Modules\Users\Http\Requests\FormRequestChangePassword;
 use TypiCMS\Modules\Users\Http\Requests\FormRequestResetPassword;
 use TypiCMS\Modules\Users\Repositories\UserInterface;
-use View;
 
-class AdminController extends BaseAdminController
+class AdminController extends Controller
 {
 
-    /**
-     * __construct
-     *
-     * @param UserInterface $user
-     */
+    protected $repository;
+
     public function __construct(UserInterface $user)
     {
-        parent::__construct($user);
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  FormRequest $request
-     * @return Redirect
-     */
-    public function store(FormRequest $request)
-    {
-        $model = $this->repository->create($request->all());
-        return $this->redirect($request, $model);
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  $model
-     * @param  FormRequest $request
-     * @return Redirect
-     */
-    public function update($model, FormRequest $request)
-    {
-        $this->repository->update($request->all());
-        return $this->redirect($request, $model);
+        $this->repository = $user;
     }
 
     public function getLogin()
@@ -79,34 +50,6 @@ class AdminController extends BaseAdminController
     {
         $this->repository->logout();
         return back();
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return Response
-     */
-    public function create()
-    {
-        $model = $this->repository->getModel();
-        $groups = $this->repository->getGroups();
-        $selectedGroups = [];
-        return view('core::admin.create')
-            ->with(compact('model', 'groups', 'selectedGroups'));
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  $model
-     * @return Response
-     */
-    public function edit($model)
-    {
-        $selectedGroups = $this->repository->getGroups($model);
-        $permissions = $model->getPermissions();
-        return view('core::admin.edit')
-            ->with(compact('model', 'selectedGroups', 'permissions'));
     }
 
     /**
@@ -281,16 +224,5 @@ class AdminController extends BaseAdminController
         return Redirect::route('login')
             ->with(compact('message'));
 
-    }
-
-    /**
-     * Update User's preferences
-     * 
-     * @return void
-     */
-    public function postUpdatePreferences()
-    {
-        $input = Input::all();
-        $this->repository->updatePreferences($input);
     }
 }
