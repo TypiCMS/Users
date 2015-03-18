68fe1a6e0e445fbd3a2e265b3f74e063b322f8cf
Samuel De Backer <sdebacker@gmail.com>
2015-03-18 23:12:38 +0100

    _index.blade

--- src/resources/views/admin/_index.blade.php
+++ src/resources/views/admin/_index.blade.php
@@ -1,8 +1,8 @@
 <div ng-app="typicms" ng-cloak ng-controller="ListController">
 
     <h1>
-        <a href="{{ url }}/create" class="btn-add"><i class="fa fa-plus-circle"></i><span class="sr-only" translate>New</span></a>
-        <span translate translate-n="models.length" translate-plural="{{ models.length }} users">{{ models.length }} user</span>
+        <a href="{{ route('admin.' . $module . '.create') }}" class="btn-add"><i class="fa fa-plus-circle"></i><span class="sr-only" translate>New</span></a>
+        <span translate translate-n="models.length" translate-plural="@{{ models.length }} users">@{{ models.length }} user</span>
     </h1>
 
     <div class="btn-toolbar" role="toolbar" ng-include="'/views/partials/btnLocales.html'"></div>
@@ -23,7 +23,7 @@
                 <tr>
                     <td colspan="2"></td>
                     <td colspan="3">
-                        <input st-search class="form-control input-sm" placeholder="{{ 'Search' | translate }}…" type="text">
+                        <input st-search class="form-control input-sm" placeholder="@{{ 'Search' | translate }}…" type="text">
                     </td>
                 </tr>
             </thead>
@@ -32,11 +32,11 @@
                 <tr ng-repeat="model in displayedModels">
                     <td><typi-btn-delete ng-click="delete(model, model.first_name + ' ' + model.last_name)"></typi-btn-delete></td>
                     <td typi-btn-edit></td>
-                    <td>{{ model.first_name }}</td>
-                    <td>{{ model.last_name }}</td>
-                    <td>{{ model.email }}</td>
-                    <td>{{ model.activated }}</td>
-                    <td>{{ model.permissions.superuser }}</td>
+                    <td>@{{ model.first_name }}</td>
+                    <td>@{{ model.last_name }}</td>
+                    <td>@{{ model.email }}</td>
+                    <td>@{{ model.activated }}</td>
+                    <td>@{{ model.permissions.superuser }}</td>
                 </tr>
             </tbody>
             <tfoot>
