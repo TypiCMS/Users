bf6593cb62c180238d5225c334b07590a50cce68
Samuel De Backer <sdebacker@gmail.com>
2015-04-24 22:21:49 +0200

    website logo in mails

--- src/resources/views/emails/reset.blade.php
+++ src/resources/views/emails/reset.blade.php
@@ -4,10 +4,12 @@
         <meta charset="utf-8">
     </head>
     <body>
+        {!! TypiCMS::logo() !!}
+
         <h2>@lang('users::global.Reset password')</h2>
 
         <p>
-            @lang('users::global.To reset your password'), 
+            @lang('users::global.To reset your password'),
             <a href="{{ route('changepassword', array('id' => $id, urlencode($code))) }}">@lang('users::global.click here').</a></p>
         <p>
             @lang('users::global.Or point your browser to this address:') <br />
--- src/resources/views/emails/welcome.blade.php
+++ src/resources/views/emails/welcome.blade.php
@@ -4,11 +4,13 @@
         <meta charset="utf-8">
     </head>
     <body>
+        {!! TypiCMS::logo() !!}
+
         <h2>@lang('users::global.Welcome') {{ $first_name }} {{ $last_name }}</h2>
 
         <p><b>@lang('users::global.Account:')</b> {{ $email }}</p>
         <p>
-            @lang('users::global.To activate your account'), 
+            @lang('users::global.To activate your account'),
             <a href="{{ route('activate', array($id, urlencode($code))) }}">@lang('users::global.click here').</a>
         </p>
         <p>
