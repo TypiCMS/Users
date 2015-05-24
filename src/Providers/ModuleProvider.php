c62389875429021c965c04ad4a76d0430739f9d0
Samuel De Backer <sdebacker@gmail.com>
2015-05-24 14:49:34 +0200

    tests publishing removed

--- src/Providers/ModuleProvider.php
+++ src/Providers/ModuleProvider.php
@@ -30,9 +30,6 @@ class ModuleProvider extends ServiceProvider
         $this->publishes([
             __DIR__ . '/../database' => base_path('database'),
         ], 'migrations');
-        $this->publishes([
-            __DIR__ . '/../../tests' => base_path('tests'),
-        ], 'tests');
 
         AliasLoader::getInstance()->alias(
             'Users',
