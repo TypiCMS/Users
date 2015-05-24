c10195256117d0a78b4e3703204e7e4b86ce7692
Samuel De Backer <sdebacker@gmail.com>
2015-05-24 22:22:55 +0200

    form request

---
+++ src/Http/Requests/FormRequestPassword.php
@@ -0,0 +1,17 @@
+<?php
+namespace TypiCMS\Modules\Users\Http\Requests;
+
+use TypiCMS\Modules\Core\Http\Requests\AbstractFormRequest;
+
+class FormRequestPassword extends AbstractFormRequest {
+
+    public function rules()
+    {
+        $rules = [
+            'token'    => 'required',
+            'email'    => 'required|email',
+            'password' => 'required|confirmed',
+        ];
+        return $rules;
+    }
+}
