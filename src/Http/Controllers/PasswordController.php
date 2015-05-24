d726db97fd71965040d384e69c8fcb4083cf3aec
Samuel De Backer <sdebacker@gmail.com>
2015-05-24 22:22:46 +0200

    pw reset

--- src/Http/Controllers/PasswordController.php
+++ src/Http/Controllers/PasswordController.php
@@ -4,28 +4,14 @@ namespace TypiCMS\Modules\Users\Http\Controllers;
 use Illuminate\Contracts\Auth\Guard;
 use Illuminate\Contracts\Auth\PasswordBroker;
 use Illuminate\Foundation\Auth\ResetsPasswords;
-use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Mail\Message;
 use Illuminate\Routing\Controller;
+use TypiCMS\Modules\Core\Facades\TypiCMS;
+use TypiCMS\Modules\Users\Http\Requests\FormRequestEmail;
+use TypiCMS\Modules\Users\Http\Requests\FormRequestPassword;
 
 class PasswordController extends Controller
 {
-
-    use ValidatesRequests;
-
-    /*
-    |--------------------------------------------------------------------------
-    | Password Reset Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller is responsible for handling password reset requests
-    | and uses a simple trait to include this behavior. You're free to
-    | explore this trait and override any methods you wish to tweak.
-    |
-    */
-
-    use ResetsPasswords;
-
-    protected $redirectPath;
 
     /**
      * Create a new password controller instance.
@@ -38,7 +24,6 @@ class PasswordController extends Controller
     {
         $this->auth = $auth;
         $this->passwords = $passwords;
-        $this->redirectPath = url('/');
 
         $this->middleware('guest');
     }
@@ -53,6 +38,28 @@ class PasswordController extends Controller
 		return view('users::password');
 	}
 
+    /**
+     * Send a reset link to the given user.
+     *
+     * @param  FormRequestEmail  $request
+     * @return Response
+     */
+    public function postEmail(FormRequestEmail $request)
+    {
+        $response = $this->passwords->sendResetLink($request->only('email'), function(Message $message) {
+            $subject  = '[' . TypiCMS::title() . '] ' . trans('users::global.Your Password Reset Link');
+            $message->subject($subject);
+        });
+
+        switch ($response) {
+            case PasswordBroker::RESET_LINK_SENT:
+                return redirect()->back()->with('status', trans($response));
+
+            case PasswordBroker::INVALID_USER:
+                return redirect()->back()->withErrors(['email' => trans($response)]);
+        }
+    }
+
 	/**
 	 * Display the password reset view for the given token.
 	 *
@@ -61,12 +68,44 @@ class PasswordController extends Controller
 	 */
 	public function getReset($token = null)
 	{
-		if (is_null($token))
-		{
+		if (is_null($token)) {
 			throw new NotFoundHttpException;
 		}
 
 		return view('users::reset')->with(compact('token'));
 	}
+
+    /**
+     * Reset the given user's password.
+     *
+     * @param  FormRequestPassword  $request
+     * @return Response
+     */
+    public function postReset(FormRequestPassword $request)
+    {
+        $credentials = $request->only(
+            'email', 'password', 'password_confirmation', 'token'
+        );
+
+        $response = $this->passwords->reset($credentials, function($user, $password) {
+            $user->password = bcrypt($password);
+
+            $user->save();
+
+            if ($user->activated) {
+                $this->auth->login($user);
+            }
+        });
+
+        switch ($response) {
+            case PasswordBroker::PASSWORD_RESET:
+                return redirect(route('login'));
+
+            default:
+                return redirect()->back()
+                            ->withInput($request->only('email'))
+                            ->withErrors(['email' => trans($response)]);
+        }
+    }
 
 }
