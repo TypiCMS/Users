b7e854127608ebf1ee8719fe257afd5e4e48ac5f
Samuel De Backer <sdebacker@gmail.com>
2015-05-24 22:42:19 +0200

    acl

--- src/Models/User.php
+++ src/Models/User.php
@@ -55,14 +55,69 @@ class User extends Base implements AuthenticatableContract, CanResetPasswordCont
     }
 
     /**
-     * Current user has access ?
+     * Returns an array of merged permissions for each
+     * group the user is in.
+     *
+     * @return array
+     */
+    public function getMergedPermissions()
+    {
+        if ( ! $this->mergedPermissions) {
+            $permissions = array();
+            foreach ($this->groups as $group) {
+                $permissions = array_merge($permissions, $group->permissions);
+            }
+            $this->mergedPermissions = array_merge($permissions, $this->permissions);
+        }
+        return $this->mergedPermissions;
+    }
+
+    /**
+     * See if a user has access to the passed permission(s).
+     * Permissions are merged from all groups the user belongs to
+     * and then are checked against the passed permission(s).
+     *
+     * Super users have access no matter what.
      *
      * @param  string|array  $permissions
-     * @param  bool  $all
      * @return bool
      */
-    public function hasAccess($permissions, $all = true)
+    public function hasAccess($permissions)
     {
+        if ($this->superuser) {
+            return true;
+        }
+        return $this->hasPermission($permissions);
+    }
+
+    /**
+     * See if a user has access to the passed permission(s).
+     * Permissions are merged from all groups the user belongs to
+     * and then are checked against the passed permission(s).
+     *
+     * Super users DON'T have access no matter what.
+     *
+     * @param  string|array  $permissions
+     * @return bool
+     */
+    public function hasPermission($permissions)
+    {
+        $mergedPermissions = $this->getMergedPermissions();
+        if ( ! is_array($permissions)) {
+            $permissions = (array) $permissions;
+        }
+        foreach ($permissions as $permission) {
+            $matched = false;
+            foreach ($mergedPermissions as $mergedPermission => $value) {
+                if ($permission == $mergedPermission and $mergedPermissions[$permission] == 1) {
+                    $matched = true;
+                    break;
+                }
+            }
+            if ($matched === false) {
+                return false;
+            }
+        }
         return true;
     }
 
