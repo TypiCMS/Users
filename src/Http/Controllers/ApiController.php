56d4a0781ddaed5983add1c16eeba1e8123a68a0
Samuel De Backer <sdebacker@gmail.com>
2015-09-25 19:46:50 +0200

    Api FormRequest

--- src/Http/Controllers/ApiController.php
+++ src/Http/Controllers/ApiController.php
@@ -4,6 +4,7 @@ namespace TypiCMS\Modules\Users\Http\Controllers;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Auth;
 use TypiCMS\Modules\Core\Http\Controllers\BaseApiController;
+use TypiCMS\Modules\Users\Http\Requests\FormRequest;
 use TypiCMS\Modules\Users\Repositories\UserInterface as Repository;
 
 class ApiController extends BaseApiController
@@ -11,6 +12,37 @@ class ApiController extends BaseApiController
     public function __construct(Repository $repository)
     {
         parent::__construct($repository);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  FormRequest $request
+     * @return Model|false
+     */
+    public function store(FormRequest $request)
+    {
+        $model = $this->repository->create(Input::all());
+        $error = $model ? false : true ;
+        return response()->json([
+            'error' => $error,
+            'model' => $model,
+        ], 200);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  $model
+     * @param  FormRequest $request
+     * @return boolean
+     */
+    public function update($model, FormRequest $request)
+    {
+        $error = $this->repository->update($request->all()) ? false : true ;
+        return response()->json([
+            'error' => $error,
+        ], 200);
     }
 
     /**
