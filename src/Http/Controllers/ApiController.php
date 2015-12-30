1f55ac23e11ae1d5748f3575ec30724e5e523363
Samuel De Backer <sdebacker@gmail.com>
2015-12-30 14:53:50 +0100

    api update and destroy methods

--- src/Http/Controllers/ApiController.php
+++ src/Http/Controllers/ApiController.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Request;
 use TypiCMS\Modules\Core\Http\Controllers\BaseApiController;
+use TypiCMS\Modules\Users\Models\User;
 use TypiCMS\Modules\Users\Repositories\UserInterface as Repository;
 
 class ApiController extends BaseApiController
@@ -38,31 +39,35 @@ class ApiController extends BaseApiController
      *
      * @return \Illuminate\Http\JsonResponse
      */
-    public function update($model)
+    public function update()
     {
-        $error = $this->repository->update(Request::all()) ? false : true;
+        $updated = $this->repository->update(Request::all());
 
         return response()->json([
-            'error' => $error,
-        ], 200);
+            'error' => !$updated,
+        ]);
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  $model
+     * @param \TypiCMS\Modules\Users\Models\User $user
      *
      * @return \Illuminate\Support\Facades\Response
      */
-    public function destroy($model)
+    public function destroy(User $user)
     {
-        if ($model->id == Auth::user()->id) {
+        if ($user->id == Auth::user()->id) {
             return response()->json([
-                'error'   => true,
+                'error' => true,
                 'message' => 'Connected user can not be deleted.',
             ], 403);
         }
 
-        return parent::destroy($model);
+        $deleted = $this->repository->delete($user);
+
+        return response()->json([
+            'error' => !$deleted,
+        ]);
     }
 }
