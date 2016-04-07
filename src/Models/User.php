7b5548cf8cba3a82c9682923dcf8f41c2c293d29
Samuel De Backer <sdebacker@gmail.com>
2016-04-07 18:00:36 +0200

    syncRoles

--- src/Models/User.php
+++ src/Models/User.php
@@ -106,13 +106,29 @@ class User extends Base implements AuthenticatableContract, AuthorizableContract
     }
 
     /**
+     * Sync roles.
+     *
+     * @param array $permissions
+     *
+     * @return null
+     */
+    public function syncRoles(array $roles)
+    {
+        $permissionIds = [];
+        foreach ($permissions as $name) {
+            $permissionIds[] = app(Permission::class)->firstOrCreate(['name' => $name])->id;
+        }
+        $this->permissions()->sync($permissionIds);
+    }
+
+    /**
      * Sync permissions.
      *
      * @param array $permissions
      *
      * @return null
      */
-    public function syncPermissions($permissions)
+    public function syncPermissions(array $permissions)
     {
         $permissionIds = [];
         foreach ($permissions as $name) {
