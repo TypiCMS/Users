54efa1495fca05ec88d4ae0695500d267b18f701
Samuel De Backer <sdebacker@gmail.com>
2016-04-12 23:02:21 +0200

    unused methods

--- src/Presenters/ModulePresenter.php
+++ src/Presenters/ModulePresenter.php
@@ -7,26 +7,6 @@ use TypiCMS\Modules\Core\Presenters\Presenter;
 class ModulePresenter extends Presenter
 {
     /**
-     * check if user is activated.
-     *
-     * @return string translated 'yes' or 'no'
-     */
-    public function activated()
-    {
-        return $this->entity->activated ? trans('global.Yes') : trans('global.No');
-    }
-
-    /**
-     * Is user superuser ?
-     *
-     * @return string translated 'yes' or 'no'
-     */
-    public function superUser()
-    {
-        return $this->entity->superuser ? trans('global.Yes') : trans('global.No');
-    }
-
-    /**
      * Get title by concatenating first_name and last_name.
      *
      * @return string
