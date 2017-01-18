909a11b41074fc58772a5717b289247d0a38cb2e
Samuel De Backer <sdebacker@gmail.com>
2017-01-18 23:36:02 +0100

    login repaired

--- src/Http/Controllers/LoginController.php
+++ src/Http/Controllers/LoginController.php
@@ -4,6 +4,8 @@ namespace TypiCMS\Modules\Users\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+use TypiCMS\Modules\Users\Models\User;
 
 class LoginController extends Controller
 {
@@ -45,5 +47,48 @@ class LoginController extends Controller
     public function showLoginForm()
     {
         return view('users::login');
+    }
+
+    /**
+     * Get the failed login response instance.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    protected function sendFailedLoginResponse(Request $request)
+    {
+        $credentials = $this->credentials($request);
+
+        $user = User::where('email', $credentials['email'])->first();
+        if (!$user) {
+            $error = 'Cet utilisateur n’a pas été trouvé.';
+        } elseif (!$user->activated) {
+            $error = 'Cet utilisateur n’a pas été activé.';
+        } else {
+            $error = 'Le mot de passe est incorrect.';
+        }
+
+        $errors = [$this->username() => $error];
+
+        if ($request->expectsJson()) {
+            return response()->json($errors, 422);
+        }
+
+        return redirect()->back()
+            ->withInput($request->only($this->username(), 'remember'))
+            ->withErrors($errors);
+    }
+
+    /**
+     * Get the needed authorization credentials from the request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    protected function credentials(Request $request)
+    {
+        $credentials = $request->only($this->username(), 'password');
+        $credentials['activated'] = 1;
+        return $credentials;
     }
 }
