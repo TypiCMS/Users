12427b3e6380a049fc101b0c113bcde6ba208d4b
Samuel De Backer <sdebacker@gmail.com>
2019-04-06 22:43:44 +0200

    Update verify.blade.php

--- src/resources/views/verify.blade.php
+++ src/resources/views/verify.blade.php
@@ -14,30 +14,22 @@
 
 @section('content')
 
-<div id="verify" class="container-verify">
+<div id="verify" class="container-verify auth-container">
 
     @include('users::_logo')
 
-    <div class="row justify-content-center">
+    <div class="auth-container-form">
 
-        <div class="col-md-8">
+        <h1 class="auth-container-title">{{ __('Verify Your Email Address') }}</h1>
 
-            <div class="card">
-
-                <div class="card-header">{{ __('Verify Your Email Address') }}</div>
-
-                <div class="card-body">
-                    @if (session('resent'))
-                        <div class="alert alert-success" role="alert">
-                            {{ __('A fresh verification link has been sent to your email address.') }}
-                        </div>
-                    @endif
-
-                    {{ __('Before proceeding, please check your email for a verification link.') }}
-                    {{ __('If you did not receive the email') }}, <a href="{{ route(app()->getLocale().'::verification.resend') }}">{{ __('click here to request another') }}</a>.
+            @if (session('resent'))
+                <div class="alert alert-success" role="alert">
+                    {{ __('A fresh verification link has been sent to your email address.') }}
                 </div>
+            @endif
 
-            </div>
+            {{ __('Before proceeding, please check your email for a verification link.') }}
+            {{ __('If you did not receive the email') }}, <a href="{{ route(app()->getLocale().'::verification.resend') }}">{{ __('click here to request another') }}</a>.
 
         </div>
 
