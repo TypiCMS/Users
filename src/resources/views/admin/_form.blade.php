6a29eb31ab094864ecf319e7b363db7d7ac9696a
Samuel De Backer <sdebacker@gmail.com>
2020-09-12 14:49:31 +0200

    id removed from the form

--- src/resources/views/admin/_form.blade.php
+++ src/resources/views/admin/_form.blade.php
@@ -1,8 +1,6 @@
 @component('core::admin._buttons-form', ['model' => $model])
 @endcomponent
 
-{!! BootForm::hidden('id') !!}
-
 <div class="row">
     <div class="col-sm-6">
         {!! BootForm::email(__('Email'), 'email')->autocomplete('off')->required() !!}
