b0675f720ad01e1baf7e22be5a3b9461cb89ba89
Samuel De Backer <sdebacker@gmail.com>
2020-09-18 14:24:03 +0200

    migrations publishable

--- src/Providers/ModuleProvider.php
+++ src/Providers/ModuleProvider.php
@@ -18,7 +18,11 @@ class ModuleProvider extends ServiceProvider
         $this->app['config']->set('typicms.modules', array_merge(['users' => []], $modules));
 
         $this->loadViewsFrom(__DIR__.'/../resources/views/', 'users');
-        $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+
+        $this->publishes([
+            __DIR__.'/../database/migrations/create_users_table.php.stub' => getMigrationFileName('create_users_table'),
+            __DIR__.'/../database/migrations/create_password_resets_table.php.stub' => getMigrationFileName('create_password_resets_table'),
+        ], 'migrations');
 
         $this->publishes([
             __DIR__.'/../resources/views' => resource_path('views/vendor/users'),
