6b2560e4bb61bace8265913a03cee80043256d8a
Samuel De Backer <sdebacker@gmail.com>
2020-09-22 16:13:59 +0200

    sidebar icon

--- src/Composers/SidebarViewComposer.php
+++ src/Composers/SidebarViewComposer.php
@@ -19,7 +19,7 @@ class SidebarViewComposer
             $group->weight = 50;
             $group->addItem(__('Users'), function (SidebarItem $item) {
                 $item->id = 'users';
-                $item->icon = config('typicms.users.sidebar.icon', 'icon fa fa-fw fa-user');
+                $item->icon = config('typicms.users.sidebar.icon');
                 $item->weight = config('typicms.users.sidebar.weight');
                 $item->route('admin::index-users');
                 $item->append('admin::create-user');
