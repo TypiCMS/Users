066bd8e6ec66b45c362ac154e0ee8084b371c1c4
Samuel De Backer <sdebacker@gmail.com>
2021-03-10 15:48:43 +0100

    Files moved

--- src/Providers/ModuleServiceProvider.php
+++ src/Providers/ModuleServiceProvider.php
@@ -18,15 +18,15 @@ class ModuleServiceProvider extends ServiceProvider
         $modules = $this->app['config']['typicms']['modules'];
         $this->app['config']->set('typicms.modules', array_merge(['users' => []], $modules));
 
-        $this->loadViewsFrom(__DIR__.'/../resources/views/', 'users');
+        $this->loadViewsFrom(__DIR__.'/../../resources/views/', 'users');
 
         $this->publishes([
-            __DIR__.'/../database/migrations/create_users_table.php.stub' => getMigrationFileName('create_users_table'),
-            __DIR__.'/../database/migrations/create_password_resets_table.php.stub' => getMigrationFileName('create_password_resets_table'),
+            __DIR__.'/../../database/migrations/create_users_table.php.stub' => getMigrationFileName('create_users_table'),
+            __DIR__.'/../../database/migrations/create_password_resets_table.php.stub' => getMigrationFileName('create_password_resets_table'),
         ], 'migrations');
 
         $this->publishes([
-            __DIR__.'/../resources/views' => resource_path('views/vendor/users'),
+            __DIR__.'/../../resources/views' => resource_path('views/vendor/users'),
         ], 'views');
 
         AliasLoader::getInstance()->alias('Users', Users::class);
