eb896b5d4bd67c3a0f25014a5bcb8434972eb714
Samuel De Backer <sdebacker@gmail.com>
2021-04-12 22:12:38 +0200

    missing route names

--- src/Providers/RouteServiceProvider.php
+++ src/Providers/RouteServiceProvider.php
@@ -26,7 +26,7 @@ class RouteServiceProvider extends ServiceProvider
                 if (config('typicms.register')) {
                     // Registration
                     $router->get('register', [RegisterController::class, 'showRegistrationForm'])->name('register');
-                    $router->post('register', [RegisterController::class, 'register']);
+                    $router->post('register', [RegisterController::class, 'register'])->name('register-action');
                     // Verify
                     $router->get('email/verify', [VerificationController::class, 'show'])->name('verification.notice');
                     $router->get('email/verify/{id}', [VerificationController::class, 'verify'])->name('verification.verify');
@@ -35,13 +35,13 @@ class RouteServiceProvider extends ServiceProvider
                 }
                 // Login
                 $router->get('login', [LoginController::class, 'showLoginForm'])->name('login');
-                $router->post('login', [LoginController::class, 'login']);
+                $router->post('login', [LoginController::class, 'login'])->name('login-action');
                 // Request new password
                 $router->get('password/reset', [ForgotPasswordController::class, 'showLinkRequestForm'])->name('password.request');
                 $router->post('password/email', [ForgotPasswordController::class, 'sendResetLinkEmail'])->name('password.email');
                 // Set new password
                 $router->get('password/reset/{token}', [ResetPasswordController::class, 'showResetForm'])->name('password.reset');
-                $router->post('password/reset', [ResetPasswordController::class, 'reset']);
+                $router->post('password/reset', [ResetPasswordController::class, 'reset'])->name('password.reset-action');
                 // Logout
                 $router->post('logout', [LoginController::class, 'logout'])->name('logout');
             });
